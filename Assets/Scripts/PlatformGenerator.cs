81d84ff86a78bc042bb81d57a356352783f0c366
Laurens Giesen <laurens.giesen@student.howest.be>
2021-03-24 22:59:10 +0100

    snelheid toevogen

--- Assets/Scripts/PlatformGenerator.cs
+++ Assets/Scripts/PlatformGenerator.cs
@@ -9,11 +9,9 @@ public class PlatformGenerator : MonoBehaviour {
     public float distanceBetween;
     private int platformSelector;
     private float platformWidth;
-    //public GameObject[] thePlatforms;
     public float distanceBetweenMin;
     public float distanceBetweenMax;
     private float[] platformWidths;
-
     public ObjectPooler[] theObjectPools;
     private float minHeight;
     public Transform maxHeightPoint;
@@ -21,21 +19,17 @@ public class PlatformGenerator : MonoBehaviour {
     public float maxHeightChange;
     private float heightChange;
     // Start is called before the first frame update
-    void Start()
-    {
-       // platformWidth = thePlatform.GetComponent<BoxCollider2D>().size.x;
+    void Start() {
        platformWidths = new float[theObjectPools.Length];
        for (int i = 0; i < theObjectPools.Length; i++) {
            platformWidths[i] = theObjectPools[i].pooledObject.GetComponent<BoxCollider2D>().size.x;
        }
        minHeight = transform.position.y;
        maxHeight = maxHeightPoint.position.y;
-
     }
 
     // Update is called once per frame
-    void Update()
-    {
+    void Update() {
         if(transform.position.x < generationPoint.position.x) {
 
             distanceBetween = Random.Range(distanceBetweenMin, distanceBetweenMax);
@@ -46,20 +40,12 @@ public class PlatformGenerator : MonoBehaviour {
             } else if (heightChange < minHeight) {
                 heightChange = minHeight;
             } 
-
-            
             transform.position = new Vector3(transform.position.x + (platformWidths[platformSelector] / 2) + distanceBetween, heightChange, transform.position.z);
-
-            //Instantiate(thePlatforms[platformSelector], transform.position, transform.rotation);
-
             GameObject newPlatform = theObjectPools[platformSelector].GetPooledObject(); 
-
             newPlatform.transform.position = transform.position;
             newPlatform.transform.rotation = transform.rotation;
             newPlatform.SetActive(true);
-
             transform.position = new Vector3(transform.position.x + (platformWidths[platformSelector] / 2), transform.position.y, transform.position.z);
         }
         }
     }
-
