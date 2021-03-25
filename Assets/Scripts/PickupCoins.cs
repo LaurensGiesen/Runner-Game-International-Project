0471626389f6b9b87bf24823a19dae14c977695d
max gryspeerdt <max.gryspeerdt@student.howest.be>
2021-03-25 13:41:40 +0100

    sound effects

--- Assets/Scripts/PickupCoins.cs
+++ Assets/Scripts/PickupCoins.cs
@@ -7,10 +7,15 @@ public class PickupCoins : MonoBehaviour
     public int scoreToGive;
 
     private ScoreManager theScoreManager;
+
+    private AudioSource coinSound;
+
     // Start is called before the first frame update
     void Start()
     {
         theScoreManager = FindObjectOfType<ScoreManager>();
+
+        coinSound =GameObject.Find("CoinSound").GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
@@ -24,6 +29,12 @@ public class PickupCoins : MonoBehaviour
             theScoreManager.AddScore(scoreToGive);
             gameObject.SetActive(false);
 
+            if(coinSound.isPlaying) {
+                coinSound.Stop();
+                coinSound.Play();
+            } else {
+                coinSound.Play();
+            }
         }
     }
 }
