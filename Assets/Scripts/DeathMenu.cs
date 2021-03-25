4519d281e89b85e732daf4b4fb60f134e42ec05a
max gryspeerdt <max.gryspeerdt@student.howest.be>
2021-03-25 11:32:19 +0100

    bring back death menu which had mysteriously disappeared and bind buttons to actions

--- Assets/Scripts/DeathMenu.cs
+++ Assets/Scripts/DeathMenu.cs
@@ -7,7 +7,7 @@ public class DeathMenu : MonoBehaviour
     public string mainMenuLevel;
 
     public void RestartGame() {
-
+        FindObjectOfType<GameManager>().Reset();
     }
 
     public void QuitToMain() {
